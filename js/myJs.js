48098c235af2f7f174d5c49a8169fb05a0adb064
ManfredHu <manfredhufe@gmail.com>
2017-01-18 20:05:54 +0800

    Site updated: 2017-01-18 20:05:53

--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     autoSetBanner();
+    //fixNavBar();
 });
 
 $(window).resize(function(){
@@ -18,20 +19,20 @@ function autoSetBanner(){
 // if($('.aboutMain').length > 0){
 //     firstL = $('h2').eq(0).offset().top;
 // }
+
+
 // $(window).scroll(function(){
+//     fixNavBar();
+// });
+
+// function fixNavBar(){
 //     if($('.aboutMain').length > 0){
 //         var sLen = $(document).scrollTop();
-//         if(sLen > firstL){
-//             $('#sidebar').css({
-//                 position: 'static'
-//             });
-//             $('.aboutMain').addClass('col-md-offset-3');
+//         if(sLen < firstL){
+//             console.log('静态');
 //         }else{
-//             $('#sidebar').css({
-//                 position: 'fixed',
-//                 top: '10px'
-//             });
-//             $('.aboutMain').removeCss('col-md-offset-3');
+//             console.log('固定');
+            
 //         }
 //     }
-// });
+// }
