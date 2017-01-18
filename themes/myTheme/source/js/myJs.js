21a5ff6c94182dafc8b28997a1b397b7eeaeab60
ManfredHu <manfredhufe@gmail.com>
2017-01-18 12:31:16 +0800

    modify text

---
+++ themes/myTheme/source/js/myJs.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    autoSetBanner();
+    fixNavBar();
+});
+
+$(window).resize(function(){
+    autoSetBanner();
+});
+
+function autoSetBanner(){
+    var $width = $(window).width();
+    var result = parseInt($width/1920*760);
+    $('.carousel').css({height: result + "px"});
+    $('.carousel .item').css({height: result + "px"});
+    $('.carousel-inner > .item > img').css({height: result + "px"});
+}
+
+// var firstL = 0;
+// if($('.aboutMain').length > 0){
+//     firstL = $('h2').eq(0).offset().top;
+// }
+
+
+// $(window).scroll(function(){
+//     fixNavBar();
+// });
+
+// function fixNavBar(){
+//     if($('.aboutMain').length > 0){
+//         var sLen = $(document).scrollTop();
+//         if(sLen < firstL){
+//             console.log('静态');
+//         }else{
+//             console.log('固定');
+            
+//         }
+//     }
+// }
